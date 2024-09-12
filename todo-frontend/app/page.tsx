7087a5a41b1434b81d97411f51297ba45ab131b2
thekaranchauhan <chauhankaran2399@gmail.com>
2024-09-12 18:38:48 -0400

    Rendered components on home page 🏠

--- todo-frontend/app/page.tsx
+++ todo-frontend/app/page.tsx
@@ -1,6 +1,15 @@
+'use client';
+
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+
 export default function Home() {
+  const refreshTasks = () => window.location.reload();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="max-w-2xl mx-auto mt-10">
+      <TaskForm refreshTasks={refreshTasks} />
+      <TaskList />
     </div>
   );
 }
